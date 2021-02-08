4ff482926e61acb45ad4d4a3190780c3bb7e0207
matthieu <matthieu.dubois@epsi.fr>
2021-02-08 11:21:44 +0100

    PremierCommit

---
+++ Index.php
@@ -0,0 +1,18 @@
+<?php
+
+$db="configuration";
+$dbhost="localhost";
+$dbport=3307;
+$dbuser="root";
+$dbpasswd="";
+
+$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+$pdo->exec("SET CHARACTER SET utf8");
+
+$stmt = $pdo->prepare("SELECT * FROM description WHERE id=1");
+$stmt->execute();
+
+$res = $stmt->fetch();
+echo $res['Description'];
+
+?>
