86aefe84ef313dd659f89db63e2c822db52fee26
matthieu <matthieu.dubois@epsi.fr>
2021-02-09 14:43:59 +0100

    ClasseTest

--- Index.php
+++ Index.php
@@ -2,9 +2,12 @@
 
 $db="configuration";
 $dbhost="localhost";
-$dbport=3306;
-$dbuser="matthieu";
-$dbpasswd="mdp";
+$dbport=3307;
+// $dbport=3306;
+$dbuser="root";
+// $dbuser="matthieu";
+$dbpasswd="";
+// $dbpasswd="mdp";
 
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 $pdo->exec("SET CHARACTER SET utf8");
--- Test.php
+++ Test.php
@@ -1,9 +1,12 @@
 <?php
 
-
 use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
+    public function testMdu (){
 
+        // $this -> assertSame(10 , 10);
+        $this -> assertEquals(10,10);
+    }
 }
