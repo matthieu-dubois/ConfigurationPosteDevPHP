2a258ecf45c1849691cb2bdce128143d71a99dc1
matthieu <matthieu.dubois@epsi.fr>
2021-02-09 15:45:19 +0100

    TestPourLavideo

--- Index.php
+++ Index.php
@@ -2,17 +2,17 @@
 
 $db="configuration";
 $dbhost="localhost";
-// $dbport=3307;
+//$dbport=3307;
 $dbport=3306;
 // $dbuser="root";
-$dbuser="matthieu";
+dbuser="matthieu";
 // $dbpasswd="";
 $dbpasswd="mdp";
 
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 $pdo->exec("SET CHARACTER SET utf8");
 
-$stmt = $pdo->prepare("SELECT * FROM description WHERE id=1");
+$stmt = $pdo->prepare("SELECT * FROM description WHERE id=2");
 $stmt->execute();
 
 $res = $stmt->fetch();
