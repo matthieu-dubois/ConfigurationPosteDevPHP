e0c94fe5fab5bb0f10e193efbfaec7033b16249e
matthieu <matthieu.dubois@epsi.fr>
2021-02-09 15:25:01 +0100

    TestServeur

--- Index.php
+++ Index.php
@@ -2,12 +2,12 @@
 
 $db="configuration";
 $dbhost="localhost";
-$dbport=3307;
-// $dbport=3306;
-$dbuser="root";
-// $dbuser="matthieu";
-$dbpasswd="";
-// $dbpasswd="mdp";
+// $dbport=3307;
+$dbport=3306;
+// $dbuser="root";
+$dbuser="matthieu";
+// $dbpasswd="";
+$dbpasswd="mdp";
 
 $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
 $pdo->exec("SET CHARACTER SET utf8");
